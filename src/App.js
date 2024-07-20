4a05d6a75d310142d1395bad256db5c09c6256cf
saurav_24 <67184162+saurav-chaudhary@users.noreply.github.com>
2024-07-20 08:32:36 -0700

    adding footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Navbar from "./components/Navbar"
 
-
+import Footer from "./components/Footer"
 function App(){
     return (
-        
+        <div>
         <Navbar/>
-        
+        <Footer/>
+        </div>
     )
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Toolbar from '@mui/material/Toolbar';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+
+const StyledFab = styled(Fab)({
+  position: 'absolute',
+  zIndex: 1,
+  top: -30,
+  left: 0,
+  right: 0,
+  margin: '0 auto',
+});
+
+function Footer() {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="fixed" sx={{ top: 'auto', bottom: 0, bgcolor: 'black' }}>
+        <Toolbar>
+          <StyledFab color="secondary" aria-label="add">
+            <AddIcon />
+          </StyledFab>
+          <Box sx={{ flexGrow: 1 }} />
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
+  );
+}
+
+export default Footer;
